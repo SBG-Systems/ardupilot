b2fc4a2497a2a5f1faf47c292d5a552dd4890973
Samuel TOLEDANO <samuel.toledano@sbg-systems.com>
2025-08-29 19:12:22 +0200

    AP_ExternalAHRS: Provide GNSS time

--- libraries/AP_ExternalAHRS/AP_ExternalAHRS_sbgECom.cpp
+++ libraries/AP_ExternalAHRS/AP_ExternalAHRS_sbgECom.cpp
@@ -33,6 +33,7 @@
 
 #include <AP_Airspeed/AP_Airspeed.h>
 #include <AP_Baro/AP_Baro.h>
+#include <AP_Common/time.h>
 #include <AP_Compass/AP_Compass.h>
 #include <AP_InertialSensor/AP_InertialSensor.h>
 #include <AP_SerialManager/AP_SerialManager.h>
@@ -361,6 +362,10 @@ SbgErrorCode AP_ExternalAHRS_sbgECom::onLogReceived(SbgEComHandle *handle, SbgEC
             process_ekf_nav_packet(ref_sbg_data, user_arg);
             break;
 
+        case SBG_ECOM_LOG_UTC_TIME:
+            process_utc_time_packet(ref_sbg_data, user_arg);
+            break;
+
         default:
             SBG_LOG_INFO("Received unhandled SBG message (class %u, id %u)", msg_class, msg);
             break;
@@ -518,12 +523,55 @@ SbgErrorCode AP_ExternalAHRS_sbgECom::sendAirDataLog(SbgEComHandle *handle)
     return error_code;
 }
 
+uint16_t AP_ExternalAHRS_sbgECom::makeGpsWeek(const SbgEComLogUtc *utc_data)
+{
+    struct tm tm {};
+
+    tm.tm_year = utc_data->year - 1900;
+    tm.tm_mon  = utc_data->month - 1;
+    tm.tm_mday = utc_data->day;
+
+    tm.tm_hour = utc_data->hour;
+    tm.tm_min = utc_data->minute;
+    tm.tm_sec = utc_data->second;
+
+    // convert from time structure to unix time
+    time_t unix_time = ap_mktime(&tm);
+
+    // convert to time since GPS epoch
+    uint32_t gps_time = unix_time - UNIX_OFFSET_MSEC / 1000;
+
+    // get GPS week
+    uint16_t gps_week = gps_time / AP_SEC_PER_WEEK;
+
+    return gps_week;
+}
+
+void AP_ExternalAHRS_sbgECom::process_utc_time_packet(const SbgEComLogUnion *ref_sbg_data, void *user_arg)
+{
+    assert(ref_sbg_data);
+    assert(user_arg);
+
+    AP_ExternalAHRS_sbgECom *instance = static_cast<AP_ExternalAHRS_sbgECom *>(user_arg);
+
+    SbgEComClockState clock_state = sbgEComLogUtcGetClockState(&ref_sbg_data->utcData);
+
+    if (clock_state == SBG_ECOM_CLOCK_STATE_VALID)
+    {
+        instance->gnss_data.gps_week = makeGpsWeek(&ref_sbg_data->utcData);
+    }
+
+    instance->gnss_data.ms_tow = ref_sbg_data->utcData.gpsTimeOfWeek;
+}
+
 void AP_ExternalAHRS_sbgECom::process_gnss_vel_packet(const SbgEComLogUnion *ref_sbg_data, void *user_arg)
 {
     assert(ref_sbg_data);
     assert(user_arg);
 
     AP_ExternalAHRS_sbgECom *instance = static_cast<AP_ExternalAHRS_sbgECom *>(user_arg);
+
+    instance->gnss_data.ms_tow = ref_sbg_data->gpsVelData.timeOfWeek;
 
     if (!instance->use_ekf_pos)
     {
@@ -544,7 +592,6 @@ void AP_ExternalAHRS_sbgECom::process_gnss_pos_packet(const SbgEComLogUnion *ref
 
     AP_ExternalAHRS_sbgECom *instance = static_cast<AP_ExternalAHRS_sbgECom *>(user_arg);
 
-    instance->gnss_data.gps_week = ref_sbg_data->gpsPosData.timeOfWeek / AP_MSEC_PER_WEEK;
     instance->gnss_data.ms_tow = ref_sbg_data->gpsPosData.timeOfWeek;
 
     instance->gnss_data.hdop = 0.0f;
@@ -583,7 +630,7 @@ void AP_ExternalAHRS_sbgECom::process_gnss_pos_packet(const SbgEComLogUnion *ref
 
         instance->gnss_data.horizontal_pos_accuracy = sqrt(ref_sbg_data->gpsPosData.latitudeAccuracy * ref_sbg_data->gpsPosData.latitudeAccuracy + ref_sbg_data->gpsPosData.longitudeAccuracy * ref_sbg_data->gpsPosData.longitudeAccuracy);
         instance->gnss_data.vertical_pos_accuracy = ref_sbg_data->gpsPosData.altitudeAccuracy;
-    
+
         instance->gnss_data.latitude = ref_sbg_data->gpsPosData.latitude * 1.0e7;
         instance->gnss_data.longitude = ref_sbg_data->gpsPosData.longitude * 1.0e7;
         instance->gnss_data.msl_altitude = ref_sbg_data->gpsPosData.altitude * 1.0e2;
@@ -629,7 +676,6 @@ void AP_ExternalAHRS_sbgECom::process_ekf_nav_packet(const SbgEComLogUnion *ref_
         instance->gnss_data.ned_vel_north = ref_sbg_data->ekfNavData.velocity[0];
         instance->gnss_data.ned_vel_east = ref_sbg_data->ekfNavData.velocity[1];
         instance->gnss_data.ned_vel_down = ref_sbg_data->ekfNavData.velocity[2];
-
     }
 
     location = Location{
